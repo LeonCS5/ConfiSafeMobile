bed92e49d82d2de99f3d9c7159bab5e59ba78deb
Leonam Candido <leonamcandido80@gmail.com>
2025-10-13 01:15:45 -0300

    update

--- app/src/main/java/com/example/confisafemobile/EpiListActivity.kt
+++ app/src/main/java/com/example/confisafemobile/EpiListActivity.kt
@@ -1,4 +1,23 @@
 package com.example.confisafemobile
 
-class EpiListActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.confisafemobile.databinding.ActivityEpiListBinding
+
+class EpiListActivity : AppCompatActivity() {
+
+    // Prepara a variável para o View Binding.
+    private lateinit var binding: ActivityEpiListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Infla o layout 'activity_epi_list.xml' e o conecta a este código.
+        binding = ActivityEpiListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Nos próximos passos, adicionaremos o código aqui para:
+        // 1. Receber os dados da área de risco vindos da WelcomeActivity.
+        // 2. Usar esses dados para exibir a lista de EPIs correta.
+    }
 }
