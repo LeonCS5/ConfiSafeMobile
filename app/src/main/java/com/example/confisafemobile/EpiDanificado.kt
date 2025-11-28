dacdd180c01fb417931d26454ca1aa8ea058f0d2
LeonCS5 <leonamcandido80@gmail.com>
2025-11-28 11:05:33 -0300

    hot fix

--- app/src/main/java/com/example/confisafemobile/EpiDanificado.kt
+++ app/src/main/java/com/example/confisafemobile/EpiDanificado.kt
@@ -38,7 +38,6 @@ class EpiDanificado : AppCompatActivity() {
         )
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, epis)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
         binding.inputEpi.adapter = adapter
 
         // Foto
@@ -47,7 +46,7 @@ class EpiDanificado : AppCompatActivity() {
         // Enviar
         binding.btnEnviar.setOnClickListener {
             val epi = binding.inputEpi.selectedItem.toString()
-            val area = binding.inputArea.text?.toString().orEmpty()
+            val area = binding.inputArea.selectedItem.toString()
             val desc = binding.inputDescricao.text?.toString().orEmpty()
             val grav = when (binding.rgGravity.checkedRadioButtonId) {
                 binding.rbHigh.id -> "Alta"
